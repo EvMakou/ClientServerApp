4fd00a103c2fe14920341f00e8c538680d05560c
supermacho <face_buk@mail.ru>
2017-10-09 13:40:51 +0300

    Initial Commit

---
+++ ClientServerApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClientServerApp
+//
+//  Created by supermacho on 09.10.17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
